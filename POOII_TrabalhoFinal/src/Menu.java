980be1b7516e4de52ae60ccf19e1f67f72d413db
davihillesheim <davi-241@hotmail.com>
2019-07-08 17:04:40 -0300

    Detalhes visuais finais do tabuleiro

--- POOII_TrabalhoFinal/src/Menu.java
+++ POOII_TrabalhoFinal/src/Menu.java
@@ -1,13 +1,12 @@
-package menuPrincipal;
 
 import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Container;
 import java.awt.Dimension;
 import java.awt.FlowLayout;
-import java.awt.Graphics;
 import java.awt.GridLayout;
-import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.image.BufferedImage;
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -18,9 +17,14 @@ import javax.swing.*;
 public class Menu extends JFrame {
 	
 	private JButton novoJogo, salvar, faca, desfaca, continuarJogo;
-	private ImagePanel panel;
+	private JPanel panel;
 	private JLabel labelLateral, labelInferior;
-	
+	private Historico historico;
+
+	public Historico getHistorico() {
+		return historico;
+	}
+
 	public Menu() {
 		
 		
@@ -49,10 +53,11 @@ public class Menu extends JFrame {
 			
 		}
 		
-		panel = new ImagePanel(bi);
-		
+		panel = new JPanel();
+		panel.setBackground(Color.black);
+
 		panel.setLayout(new BorderLayout());
-		panel.setBackground(Color.getHSBColor(60, 10, 96));
+		panel.setBackground(new Color(82, 48, 0));
 		
 		Container cont1 = new Container();
 		cont1.setLayout(new FlowLayout());
@@ -69,10 +74,77 @@ public class Menu extends JFrame {
 		panel.add(cont1, BorderLayout.NORTH);
 		panel.add(labelLateral, BorderLayout.EAST);
 		panel.add(labelLateral, BorderLayout.WEST);
-		panel.add(labelInferior, BorderLayout.SOUTH);
-	
+//		panel.add(labelInferior, BorderLayout.SOUTH);
+
+		Posicao[][] posicao = Tabuleiro.GetInstance().getPosicoes();
+		TabuleiroFrame tf = new TabuleiroFrame(Tabuleiro.GetInstance().getPosicoes());
+//		tf.setPreferredSize(new Dimension(500, 500));
+//		tf.setMaximumSize(new Dimension(500, 500));
+		ImagePanel wrapper = new ImagePanel(bi);
+		wrapper.add(tf);
+		wrapper.setBackground(Color.LIGHT_GRAY);
+		panel.add(wrapper, BorderLayout.CENTER);
+
+		historico = new Historico(tf);
+		Tabuleiro.GetInstance().informarHistorico(historico);
+
+		novoJogo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Tabuleiro.novaInstancia();
+				historico = new Historico(tf);
+				Tabuleiro.GetInstance().informarHistorico(historico);
+				tf.revisarTodo();
+			}
+		});
+
+		desfaca.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				getHistorico().desfazer();
+				tf.revizarTabuleiro();
+			}
+		});
+
+		faca.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				try {
+					Jogada j = getHistorico().refazer();
+					Dimension dim = j.pegarPosicaoNova().GetDimension();
+					tf.desgrifarQuadrado(dim.width, dim.height, Tabuleiro.GetInstance().getPosicoes());
+					tf.desgrifarQuadrado(dim.height, dim.width, Tabuleiro.GetInstance().getPosicoes());
+					dim = j.pegarPosicaoAtual().GetDimension();
+					tf.desgrifarQuadrado(dim.width, dim.height, Tabuleiro.GetInstance().getPosicoes());
+					tf.desgrifarQuadrado(dim.height, dim.width, Tabuleiro.GetInstance().getPosicoes());
+				} catch (NullPointerException ex) {
+					ex.printStackTrace();
+				}
+			}
+		});
+
+		salvar.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				historico.salvarHistorico("Teste.txt");
+			}
+		});
+
+		continuarJogo.addActionListener(new ActionListener() {
+			@Override
+			public void actionPerformed(ActionEvent e) {
+				Tabuleiro.novaInstancia();
+				historico = new Historico(tf);
+				Tabuleiro.GetInstance().informarHistorico(historico);
+				historico.carregarHistorico("Teste.txt");
+				tf.revisarTodo();
+			}
+		});
+
 		setVisible(true);
 		setSize(700,550);
+		setResizable(false);
+		setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
 	}
 
 	public static void main(String[] args) {
@@ -80,19 +152,19 @@ public class Menu extends JFrame {
 		menu.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 	}
 }
-
-class ImagePanel extends JPanel {
-	
-	Image bgImage;
-	
-	public ImagePanel(BufferedImage bi) {
-		bgImage = bi;
-	}
-	
-	@Override
-	  protected void paintComponent(Graphics g) {
-
-	    super.paintComponent(g);
-	        g.drawImage(bgImage, 0, 0, null);
-	}
-}
+//
+//class ImagePanel extends JPanel {
+//
+//	Image bgImage;
+//
+//	public ImagePanel(BufferedImage bi) {
+//		bgImage = bi;
+//	}
+//
+//	@Override
+//	  protected void paintComponent(Graphics g) {
+//
+//	    super.paintComponent(g);
+//	        g.drawImage(bgImage, 0, 0, null);
+//	}
+//}
